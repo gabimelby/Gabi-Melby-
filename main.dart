3f8dae82ca6adf2773ad9a0206cccc16e95443fd
Gabi Melby <melbyg@huntington.edu>
2025-04-11 15:11:57 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
+//Gabi Melby
+//used chatgpt and deep seek to help me create my classes for my project. 
+//also had them help me create my controller and models. 
+
+
 import 'package:flutter/material.dart';
 import 'views/kill_tracker_screen.dart';
 
