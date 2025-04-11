adc83f2e912b3cdbc7306a4227bdad406d4c97ce
Gabi Melby <melbyg@huntington.edu>
2025-04-11 15:12:12 -0400

    Update kill_tracker_screen.dart

--- views/kill_tracker_screen.dart
+++ views/kill_tracker_screen.dart
@@ -1,3 +1,8 @@
+//Gabi Melby
+//used chatgpt and deep seek to help me create my classes for my project. 
+//also had them help me create my controller and models. 
+
+
 import 'package:flutter/material.dart';
 import '../../controllers/kill_tracker_controller.dart';
 
